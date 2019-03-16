ab8a5d9c0ed78fffb1c7a1508912b68169ca1959
Christopher Harrison <cnharrison@gmail.com>
2019-03-16 13:52:07 -0400

    iterate over dummy data, setup post container, console log props

---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+function PostContainer(props) { 
+    console.log(props);
+    return ( 
+        <div>
+            <h2>Post container yo!</h2>
+        </div>
+    )
+}
+
+export default PostContainer; 
