bcf61df0f3a9ed33b809f8031cb28f8a9eb35b8b
Christopher Harrison <cnharrison@gmail.com>
2019-03-18 14:49:05 -0400

    add placeholder submit comment form

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Comment from "../Comment/Comment"
+import Comment from "../Comment/Comment";
 
 function CommentSection(props) {
   console.log(props);
@@ -8,6 +8,10 @@ function CommentSection(props) {
       {props.comments.map(comment => (
         <Comment comment={comment} />
       ))}
+      <form>
+        <input name="addPost" placeholder="Add a comment"/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
