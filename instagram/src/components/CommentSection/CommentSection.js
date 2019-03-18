becf99befde206d57a65b7d3529cbd6a2efddfcf
Christopher Harrison <cnharrison@gmail.com>
2019-03-18 15:55:24 -0400

    add proptypes on CommentSection

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Comment from "../Comment/Comment";
+import PropTypes from "prop-types";
 
 function CommentSection(props) {
   console.log(props);
@@ -9,11 +10,20 @@ function CommentSection(props) {
         <Comment comment={comment} key={index} />
       ))}
       <form>
-        <input name="addPost" placeholder="Add a comment"/>
+        <input name="addPost" placeholder="Add a comment" />
         <button type="submit">Submit</button>
       </form>
     </div>
   );
 }
+
+CommentSection.propTypes = {
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      username: PropTypes.string.isRequired
+    })
+  )
+};
 
 export default CommentSection;
